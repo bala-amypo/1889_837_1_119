a3eb9b4e2f86bc8ae2853a2de468e51f99030f5f
bala-amypo <bala@amypo.in>
2025-12-22 02:19:04 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskAnalysisService.java
+++ src/main/java/com/example/demo/service/RiskAnalysisService.java
@@ -1,2 +1,9 @@
 package com.example.demo.service;
-public interface RiskAnalysisService {}
+
+import com.example.demo.model.RiskAnalysisResult;
+import java.util.List;
+
+public interface RiskAnalysisService {
+
+    List<RiskAnalysisResult> getAnalysesForPortfolio(Long portfolioId);
+}
---
+++ src/main/java/com/example/demo/service/Serviceimpl/RiskThresholdServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.serviceimpl;
+
+import com.example.demo.model.RiskThreshold;
+import com.example.demo.repository.RiskThresholdRepository;
+import com.example.demo.service.RiskThresholdService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RiskThresholdServiceImpl implements RiskThresholdService {
+
+    private final RiskThresholdRepository repository;
+
+    public RiskThresholdServiceImpl(RiskThresholdRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public RiskThreshold getThresholdForPortfolio(Long portfolioId) {
+        return null;
+    }
+}
