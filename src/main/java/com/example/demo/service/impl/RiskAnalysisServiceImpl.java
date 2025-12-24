283962231960810a6205f0f1242f23e5c478eb1f
bala-amypo <bala@amypo.in>
2025-12-24 04:45:58 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/RiskAnalysisServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.RiskAnalysisResult;
+import com.example.demo.service.RiskAnalysisService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RiskAnalysisServiceImpl implements RiskAnalysisService {
+
+    @Override
+    public List<RiskAnalysisResult> getAnalysesForPortfolio(Long portfolioId) {
+        return List.of();
+    }
+}
